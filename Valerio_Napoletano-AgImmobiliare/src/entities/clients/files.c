d8561fd3af45a7735b6185f593d0f5c87cc5f0e2
Saverio Valerio <saverio.valerio@outlook.it>
2019-05-22 22:00:48 +0200

    Fix client_type and budget parsing, add reg_date parsing logic (Fix #1), update client.csv data

--- Valerio_Napoletano-AgImmobiliare/src/entities/clients/files.c
+++ Valerio_Napoletano-AgImmobiliare/src/entities/clients/files.c
@@ -23,6 +23,9 @@ void readClientFile(FILE *filePtr, clients *cl) {
 	// Client counter
 	int cl_num = 0;
 
+	// Temp var for string to integer conversion (enum)
+	int enum_tmp = 0;
+
 	while (fgets(line, sizeof line, filePtr) != NULL) /* read a line */
 	{
 		// Fields counter (ID, name, etc..)
@@ -44,10 +47,8 @@ void readClientFile(FILE *filePtr, clients *cl) {
 				strcpy(cl[cl_num].surname, token);
 				break;
 			case 3:
-				//TODO: Complete
-				// Enum - Client type
-				//cl[cl_num].cl_type = token;
-				cl[cl_num].cl_type = family;
+				enum_tmp = atoi(token);
+				cl[cl_num].cl_type = enum_tmp;
 				break;
 			case 4:
 				strcpy(cl[cl_num].company_name, token);
@@ -56,15 +57,15 @@ void readClientFile(FILE *filePtr, clients *cl) {
 				cl[cl_num].budget = atoi(token);
 				break;
 			case 6:
-				//TODO: Complete
-				//Date
-				//strcpy(cl[cl_num].date, token);
+				/**
+				 * Parse date in Italian format (day/month/year)
+				 */
+				sscanf(token, "%hu/%hu/%hu", &cl[cl_num].reg_date.day,
+						&cl[cl_num].reg_date.month, &cl[cl_num].reg_date.year);
 				break;
 			case 7:
-				//TODO: Complete
-				// Enum - Building type
-				//cl[cl_num].building_type = token;
-				cl[cl_num].building_type = flat;
+				enum_tmp = atoi(token);
+				cl[cl_num].building_type = enum_tmp;
 				break;
 			}
 
