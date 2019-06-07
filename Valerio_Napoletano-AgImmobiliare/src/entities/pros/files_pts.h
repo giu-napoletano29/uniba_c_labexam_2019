271e79aa471a5c9a867a0e4a346e83335e73bde4
Saverio Valerio <saverio.valerio@outlook.it>
2019-06-07 21:02:41 +0200

    Add missing Doxygen header docs to files_pts.h

--- Valerio_Napoletano-AgImmobiliare/src/entities/pros/files_pts.h
+++ Valerio_Napoletano-AgImmobiliare/src/entities/pros/files_pts.h
@@ -1,12 +1,11 @@
-/*
- * files_pts.h
- *
- *  Created on: 6 giu 2019
- *      Author: Saverio Valerio
+/**
+ * @file files_pts.h
+ * @author Saverio Valerio
+ * @date 6 June 2019
+ * @brief Header file with prototypes of functions related to the professionals' "potential".
  */
 
 void findPotential(char id[], potential *pr, int num_profess);
-
 void parsePotentialsFile(FILE *fp_pot, potential *pr);
 void loadPotentialsFile(potential *allPotentials);
 int appendPtsToFile(potential *pt);
