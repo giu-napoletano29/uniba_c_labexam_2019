cca560ffa9359f12203cbf0b9d35e56f3dc16a10
Saverio Valerio <saverio.valerio@outlook.it>
2019-06-09 13:48:29 +0200

    Add resource code and a new shiny "house" icon for the program

---
+++ Valerio_Napoletano-AgImmobiliare/src/resources.h
@@ -0,0 +1,12 @@
+/**
+ * @file resources.h
+ * @author Saverio Valerio
+ * @date 9 June 2019
+ * @brief Header file containing the AppIcon definition.
+ */
+#ifndef RESOURCES_H_
+#define RESOURCES_H_
+
+#define AppIcon 101
+
+#endif /* RESOURCES_H_ */
